32719ca805447f2b8822101f0fb563affb75e880
Michael Vanderpoorten <michael@yungpanda.com>
2025-09-25 16:59:25 +0200

    Week 1

---
+++ week-1/react-basics/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Title from "./components/Title";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const color = "red";
+
+  const items = ["Item 1", "Item 2"];
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <ul>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <Title color={color}>Dit is mijn titel</Title>
+      <Title color="orange">Dit is mijn titel</Title>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+};
+
+export default App;
---
+++ week-1/react-basics/src/components/Title.jsx
@@ -0,0 +1,5 @@
+const Title = ({ color, children }) => {
+  return <h1 style={{ color: color }}>{children}</h1>;
+};
+
+export default Title;
